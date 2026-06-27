3e165134b86fbe26bb8839b33b6d663b36cfb077
Kenia Esmeralda Ramos Javier <esme.E_N.tb@outlook.com>
2026-06-26 21:09:15 -0600

    feat: fix the routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 import MapPage from "./pages/MapPage";
 import BoardPage from "./pages/BoardPage";
-import ReportPage from "./pages/ReportPage";
 import UrgentPage from "./pages/UrgentPage";
 import PetsPage from "./pages/PetsPage";
 import SpeciesPage from "./pages/SpeciesPage";
+import ReportPage from "./pages/ReportPage";
+import ProfilePage from "./pages/ProfilePage";
+
 import "./styles/index.css";
 
 function App() {
@@ -17,12 +20,11 @@ function App() {
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/map" element={<MapPage />} />
         <Route path="/board" element={<BoardPage />} />
-        <Route path="/report" element={<ReportPage />} />
         <Route path="/urgent" element={<UrgentPage />} />
         <Route path="/pets" element={<PetsPage />} />
         <Route path="/species" element={<SpeciesPage />} />
-
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/report" element={<ReportPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
